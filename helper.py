2fa69e8616dbd29f1a5994cf4044664484da0720
Lalo V <laloalchemist@outlook.com>
2023-08-14 13:58:18 -0600

    commit9

--- helper.py
+++ helper.py
@@ -146,22 +146,7 @@ def play_rtsp_stream(conf, model):
             st.sidebar.error("Error loading RTSP stream: " + str(e))
 
 
-def _display_detected_frames(conf, model, image, is_display_tracking=None, tracker=None):
-    image = cv2.resize(image, (720, int(720*(9/16))))
-    if is_display_tracking:
-        res = model.track(image, conf=conf, persist=True, tracker=tracker)
-    else:
-        # Predict the objects in the image using the YOLOv8 model
-        res = model.predict(image, conf=conf)
-    # Return the processed frame to callback
-    res_plotted = res[0].plot()
-    return res_plotted
 
-def callback(frame):
-    img = frame.to_ndarray(format="bgr24")
-    is_display_tracker, tracker = display_tracker_options()
-    image = _display_detected_frames(img, is_display_tracker, tracker)
-    return av.VideoFrame.from_ndarray(image, format="bgr24")
 
 def play_webcam():
     """
@@ -179,7 +164,6 @@ def play_webcam():
     """
     webrtc_streamer(
         key="example",
-        video_frame_callback=callback,
         rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
     )
 
