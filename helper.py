5f2427083cdc69b2dca8cb77e7f694bb47e50200
Lalo V <laloalchemist@outlook.com>
2023-08-14 14:05:52 -0600

    commitclass1

--- helper.py
+++ helper.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer
+from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
 import numpy as np
 from PIL import Image
 import av
@@ -148,13 +148,9 @@ def play_rtsp_stream(conf, model):
 
 
 
-def play_webcam():
+def play_webcam(conf, model):
     """
-    Plays a webcam stream. Detects Objects in real-time using the YOLOv8 object detection model.
-
-    Parameters:
-        conf: Confidence of YOLOv8 model.
-        model: An instance of the `YOLOv8` class containing the YOLOv8 model.
+    Plays a webcam stream. Detects Objects in real-time using the YOLO object detection model.
 
     Returns:
         None
@@ -162,10 +158,41 @@ def play_webcam():
     Raises:
         None
     """
+    st.sidebar.title("Webcam Object Detection")
+
     webrtc_streamer(
         key="example",
+        video_transformer_factory=lambda: MyVideoTransformer(conf, model),
         rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+        media_stream_constraints={"video": True, "audio": False},
     )
+
+class MyVideoTransformer(VideoTransformerBase):
+    def __init__(self, conf, model):
+        self.conf = conf
+        self.model = model
+
+    def recv(self, frame):
+        image = frame.to_ndarray(format="bgr24")
+        processed_image = self._display_detected_frames(image)
+        st.image(processed_image, caption='Detected Video', channels="BGR", use_column_width=True)
+
+    def _display_detected_frames(self, image):
+        orig_h, orig_w = image.shape[0:2]
+        width = 720  # Set the desired width for processing
+
+        # cv2.resize used in a forked thread may cause memory leaks
+        input = np.asarray(Image.fromarray(image).resize((width, int(width * orig_h / orig_w))))
+
+        if self.model is not None:
+            # Perform object detection using YOLO model
+            res = self.model.predict(input, conf=self.conf)
+
+            # Plot the detected objects on the video frame
+            res_plotted = res[0].plot()
+            return res_plotted
+
+        return input
 
 
 
